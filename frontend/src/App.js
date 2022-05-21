e1bcd7e784a20bedcb73235f3049bbd574a29f67
springyboiii <sugumararvinth19991018@gmail.com>
2022-05-21 15:02:48 +0530

    backend working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,41 @@ import PublicLayout from "./LAYOUT/PublicLayout";
 import AdminLayout from "./LAYOUT/AdminLayout";
 import Login from "./components/login/Login";
 import bloodbanks from "./bloodbanks";
-import { useState } from "react";
+import React, { useState } from "react";
 import campaigns from "./campaigns";
+// import { render } from "../../backend/app";
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={apiResponse:"",bloodbanksState : bloodbanks,campaignsState :campaigns };
+  }
+  callAPI(){
+    fetch("http://localhost:9000/test")
+    .then(res =>res.text())
+    .then(res =>this.setState({apiResponse : res}))
 
-function App() {
-  const [bloodbanksState, setBloodBanks] = useState(bloodbanks);
-  const [campaignsState,setCampaigns]=useState(campaigns);
+  }
+  componentWillMount(){
+    this.callAPI();
+  }
+
+render(){
+  // const [bloodbanksState, setBloodBanks] = useState(bloodbanks);
+  // const [campaignsState,setCampaigns]=useState(campaigns);
   return (
+    
     <Router>
       <Routes>
         <Route path="/signIn*" element={<Login />} />
-        <Route path="/admin/*" element={<AdminLayout bloodbanks={bloodbanksState} />} />
-        <Route path="/*" element={<PublicLayout bloodbanks={bloodbanksState} campaigns={campaigns}/>} />
+        <Route path="/admin/*" element={<AdminLayout bloodbanks={this.state.bloodbanksState} />} />
+        <Route path="/*" element={<PublicLayout bloodbanks={this.state.bloodbanksState} campaigns={this.state.campaignsState}/>} />
       </Routes>
     </Router>
   );
+  return(
+  <p>{this.state.apiResponse}</p>
+  )
+}
 }
 
 export default App;
