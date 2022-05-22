cb56b742defe0964d148b18a6df3488fe268dd39
Sanduni-Ayesha <sanduniayesha.19@cse.mrt.ac.lk>
2022-05-22 13:52:54 +0530

    mysql connect
    Connected to mysql and DDL addded for reference with changes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'Sandu@123',
+  database: 'bloodbanks'
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  db.query("INSERT INTO bloodbanks_bloodbanks (title,description,completed) VALUES ('Colombo', 'Available', 1);", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
   res.render('index', { title: 'Express' });
 });
 
