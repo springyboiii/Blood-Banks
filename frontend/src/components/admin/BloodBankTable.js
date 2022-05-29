e612b1cfcb3be656120bfd1d84f239059c8ba6e8
springyboiii <sugumararvinth19991018@gmail.com>
2022-05-29 17:17:58 +0530

    added admin dashboard

--- frontend/src/components/admin/BloodBankTable.js
+++ frontend/src/components/admin/BloodBankTable.js
@@ -1,75 +1,68 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import Axios from "axios";
 const BloodBankTable = (props) => {
-  let { bloodBankList } = props;
-  const [data, setData] = useState("");
+  // let { bloodBankList } = props;
+  
+  const [bloodBankList, setBloodBankList] = useState([]);
   useEffect(() => {
-    var filter, table, tr, td, i, txtValue;
-    var rowList = [];
-    filter = data.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
+    return () => {
+      Axios.get("http://localhost:9000/dashboard").then((response) => {
+        console.log(response.data);
+        setBloodBankList(response.data)
+      });
+    };
+  }, []);
+  // useEffect(() => {
+  //   var filter, table, tr, td, i, txtValue;
+  //   var rowList = [];
+  //   filter = data.toUpperCase();
+  //   table = document.getElementById("myTable");
+  //   tr = table.getElementsByTagName("tr");
 
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td");
-      for (let j = 0; j < td.length - 2; j++) {
-        let element = td[j];
-        if (element) {
-          txtValue = element.textContent || element.innerText;
-          if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            if (!rowList.includes(i)) {
-              rowList.push(i);
-            }
-          }
-        }
-      }
-    }
-    // console.log(rowList);
-    for (let index = 1; index < tr.length; index++) {
-      if (rowList.includes(index) && tr[index]) {
-        tr[index].style.display = "";
-      } else {
-        tr[index].style.display = "none";
-      }
-    }
-  }, [data]);
+  //   for (i = 0; i < tr.length; i++) {
+  //     td = tr[i].getElementsByTagName("td");
+  //     for (let j = 0; j < td.length - 2; j++) {
+  //       let element = td[j];
+  //       if (element) {
+  //         txtValue = element.textContent || element.innerText;
+  //         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+  //           if (!rowList.includes(i)) {
+  //             rowList.push(i);
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  //   // console.log(rowList);
+  //   for (let index = 1; index < tr.length; index++) {
+  //     if (rowList.includes(index) && tr[index]) {
+  //       tr[index].style.display = "";
+  //     } else {
+  //       tr[index].style.display = "none";
+  //     }
+  //   }
+  // }, [data]);
   return (
     <>
       <div>
-        <h1 className="h1" style={{ textAlign: "center" }}>Dashborad</h1>
+        <h1 className="h1" style={{ textAlign: "center" }}>Dashboard</h1>
         <div className="row">
           <div className="col-1">
-            <label htmlFor="myInput">
-              <i
-                className="material-icons"
-                style={{ marginLeft: "120%", marginTop: "50%" }}
-              >
-                search
-              </i>
-            </label>
+            
           </div>
-          <div className="col-11">
-            <input
-              type="text"
-              id="myInput"
-              className="input"
-              onChange={(e) => {
-                setData(e.target.value);
-              }}
-              placeholder="Search..."
-              title="Type in a name"
-            />
-          </div>
+          
         </div>
         <table id="myTable">
           <thead>
             <tr className="header">
               <th style={{ width: "25%" }}>Id</th>
               <th style={{ width: "25%" }}>Name</th>
+              <th style={{ width: "25%" }}>Username</th>
               <th style={{ width: "25%" }}>Location</th>
-              <th style={{ width: "25%" }}>contact</th>
-              <th style={{ width: "5%" }}></th>
-              <th style={{ width: "5%" }}></th>
+              <th style={{ width: "25%" }}>Contact</th>
+              {/* <th style={{ width: "5%" }}></th>
+              <th style={{ width: "5%" }}></th> */}
             </tr>
           </thead>
           <tbody>
@@ -77,20 +70,12 @@ const BloodBankTable = (props) => {
               bloodBankList.map((bloodBank) => {
                 return (
                   <tr key={bloodBank._id}>
-                    <td>{bloodBank._id}</td>
+                    <td>{bloodBank.ID}</td>
                     <td>{bloodBank.name}</td>
+                    <td>{bloodBank.username}</td>
                     <td>{bloodBank.address}</td>
-                    <td>{bloodBank.telephone}</td>
-                    <td>
-                      <Link to={`/admin/dashboard/${bloodBank.id}/edit`}>
-                        <i className="material-icons">edit</i>
-                      </Link>
-                    </td>
-                    <td>
-                      <Link to={`/admin/dashboard/${bloodBank.id}/delete`}>
-                        <i className="material-icons">delete</i>
-                      </Link>
-                    </td>
+                    <td>{bloodBank.contact_no}</td>
+                   
                   </tr>
                 );
               })}
